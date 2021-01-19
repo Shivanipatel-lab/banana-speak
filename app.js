d23e221d9d639409ea0d91b8117730e19b1fda3a
Shivani Patel <shivani.patel105556@marwadiuniversity.ac.in>
2021-01-19 18:48:01 +0530

    done banana speak

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+	
 
-btnTranslate.addEventListener("click",function clickEventHandler()
+function getTranslationURL(text)
 {
-      console.log("clicked !!")
-
+    return serverURL  + "?" + "text=" + text
 }
-)
+
+
+function errorHandler(error)
+{
+      console.log("error occured" ,error);
+      alert("something wrong with server! try again after some time")
+}
+
+function clickHandler() {
+      
+      var inputText = txtInput.value;
+      fetch(getTranslationURL(inputText))
+      .then(response => response.json())
+      .then(json => {
+          var translatedText = json.contents.translated;
+          outputDiv.innerText = translatedText; // output
+         })
+      .catch(errorHandler)
+
+
+      };
+btnTranslate.addEventListener("click",clickHandler)
+
+
